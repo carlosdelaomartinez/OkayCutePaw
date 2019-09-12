3f245ab72da308635df4408851162e07dad5c0d4
Carlos Delao Martinez <delao.carlos@gmail.com>
2019-09-11 21:45:03 -0700

    working on the querying in the controller

--- app/controllers/api/questions_controller.rb
+++ app/controllers/api/questions_controller.rb
@@ -1,2 +1,12 @@
 class Api::QuestionsController < ApplicationController
+
+  def show
+    @question = Question.where('id NOT IN ?', QuestionAnswer.where('user_id IS NOT ?', current_user.id))
+  end
+
+  def index
+
+  end
 end
+
+# QuestionAnswer.where(user_id: 100).pluck(:question_id)
