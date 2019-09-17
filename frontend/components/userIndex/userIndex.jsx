f8bb9276e0d63b6bd44a7e5551de2613ce6e977b
Carlos Delao Martinez <delao.carlos@gmail.com>
2019-09-17 10:33:51 -0700

    User Card Slider works

--- frontend/components/userIndex/userIndex.jsx
+++ frontend/components/userIndex/userIndex.jsx
@@ -6,6 +6,8 @@ class UserIndex extends React.Component {
   constructor(props){
     super(props)
     this.generateUserList = this.generateUserList.bind(this);
+    this.handleBrowse = this.handleBrowse.bind(this);
+    this.browse = 0;
   }
   generateUserList(){
     const {currentUser} = this.props;
@@ -24,6 +26,54 @@ class UserIndex extends React.Component {
         .map((user, i) => <UserShowCard user={user} key={i}/>)
     }
   }
+  handleBrowse(form){
+    return (e) => {
+      let slider = e.target.closest('.user-slider-outer').children[1].firstChild 
+      let leftBtn = e.target.closest('.user-slider-outer').children[0]
+      let rightBtn = e.target.closest('.user-slider-outer').children[3]
+      let fade = e.target.closest('.user-slider-outer').children[2]
+      if( this.browse === 0 && form === 'right'){
+        leftBtn.classList.remove('browse-btn-hide');
+        slider.classList.remove('slide0');
+        slider.classList.add('slide1');
+        fade.classList.add('fade');
+        this.browse += 1;
+      } else if (this.browse === 1 && form === 'right'){
+        slider.classList.remove('slide1')
+        slider.classList.add('slide2')
+        this.browse += 1;
+      } else if (this.browse === 2 && form === 'right'){
+        slider.classList.remove('slide2')
+        slider.classList.add('slide3')
+        this.browse += 1;
+      } else if (this.browse === 3 && form === 'right'){
+        slider.classList.remove('slide3')
+        slider.classList.add('slide4')
+        this.browse += 1;
+        rightBtn.classList.add('browse-btn-hide')
+      } else if (this.browse === 4 && form === 'left'){
+        slider.classList.remove('slide4')
+        slider.classList.add('slide3')
+        this.browse -= 1;
+        rightBtn.classList.remove('browse-btn-hide')
+      } else if (this.browse === 3 && form === 'left') {
+        slider.classList.remove('slide3')
+        slider.classList.add('slide2')
+        this.browse -= 1; 
+      } else if (this.browse === 2 && form === 'left') {
+        slider.classList.remove('slide2')
+        slider.classList.add('slide1')
+        this.browse -= 1;
+      } else if (this.browse === 1 && form === 'left') {
+        leftBtn.classList.add('browse-btn-hide');
+        slider.classList.remove('slide1');
+        slider.classList.add('slide0');
+        fade.classList.remove('fade');
+        this.browse -= 1;
+      }
+    }
+  }
+
 
   render(){
     return (
@@ -34,16 +84,16 @@ class UserIndex extends React.Component {
           </div>  
           <div className='user-discovery-content-row'>
             <div className='user-slider-outer'>
-              <button className="browse-left browse-btn" >
+              <button className="browse-left browse-btn browse-btn-hide" onClick={this.handleBrowse('left')}>
                 <img src={window.leftarrowURL} />
               </button>
               <div className="slider-container">
-                <div className='user-slider-inner'>
+                <div className='user-slider-inner slide0'>
                   {this.generateUserList()}
                 </div>
               </div>
-                    
-              <button className="browse-right browse-btn">
+              <div className='browse-fade-border'></div>    
+              <button className="browse-right browse-btn" onClick={this.handleBrowse('right')}>
                 <img src={window.rightarrowURL} alt="" />
               </button>          
 
